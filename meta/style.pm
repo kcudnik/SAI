5725c56a43293227a76438cfa7e70dc6d34ba400
kcudnik <kcudnik@gmail.com>
2019-10-21 19:16:47 +0200

    Add metadata iscallback flag

--- meta/style.pm
+++ meta/style.pm
@@ -431,7 +431,7 @@ sub CheckFunctionNaming
     my $typename = $1;
     my $name = $2;
 
-    if ($name =~ /^(recv_hostif_packet|send_hostif_packet|flush_fdb_entries|remove_all_neighbor_entries|profile_get_value|profile_get_next_value)$/)
+    if ($name =~ /^(recv_hostif_packet|send_hostif_packet|flush_fdb_entries|remove_all_neighbor_entries|profile_get_value|profile_get_next_value|switch_register_read|switch_register_write)$/)
     {
         # ok
     }
@@ -461,7 +461,8 @@ sub CheckFunctionNaming
     if (not $name =~ /^(create|remove|get|set)_\w+?(_attribute)?$|^clear_\w+_stats$/)
     {
         # exceptions
-        return if $name =~ /^(recv_hostif_packet|send_hostif_packet|flush_fdb_entries|profile_get_value|profile_get_next_value)$/;
+        return if $name =~ /^(recv_hostif_packet|send_hostif_packet|flush_fdb_entries|profile_get_value|profile_get_next_value|switch_register_read|switch_register_write)$/;
+
 
         LogWarning "function not follow convention in $header:$n:$line";
     }
@@ -801,7 +802,8 @@ sub CheckHeadersStyle
             {
                 my $param = $1;
 
-                my $pattern = '^(attr_count|object_count|number_of_counters|count|u32)$';
+                my $pattern = '^(attr_count|object_count|number_of_counters|count|u32|device_addr|start_reg_addr|number_of_registers|reg_val)$';
+
 
                 if (not $param =~ /$pattern/)
                 {
